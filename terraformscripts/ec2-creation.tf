827673058f946fd379a605e0ca81b789bc767d8a
Bharath Chandra <bharathchandra6368@gmail.com>
2023-11-10 16:03:49 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "AWSServer" {
 	 key_name = "bharathmediumtechnologies"
 	 security_groups = ["launch-wizard-2"]
 	 tags = {
-	 Name = "tomcatserver"
+	 Name = "tomcatservers"
 	 }
 }
 
