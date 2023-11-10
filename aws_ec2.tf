f07d9daf2f62918dd5b1e1383ca08ff5017e253b
Bharath Chandra <bharathchandra6368@gmail.com>
2023-11-10 16:06:49 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,15 +1,13 @@
-
 provider "aws"{
 region = "ap-south-1"
 }
 
 resource "aws_instance" "AWSServer" {
-     count ="3"
-     ami = "ami-02e94b011299ef128"
-	 instance_type = "t2.micro"
-	 key_name = "bharathmediumtechnologies"
-	 security_groups = ["launch-wizard-2"]
-	 tags = {
-	 Name = "tomcatservers"
-	 }
+ami = "ami-02e94b011299ef128"
+instance_type = "t2.micro"
+key_name = "bharathmediumtechnologies"
+security_groups = ["launch-wizard-2"]
+tags = {
+Name = "tomcatservers"
+}
 }
